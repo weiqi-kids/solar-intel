2d4b97e6c34abf9084e34fd6d82e5d022503a42b
Light <lightman.chang@gmail.com>
2026-04-26 11:08:26 +0800

    feat: add Playwright fetchers for all tracked companies

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -1,6 +1,49 @@
 """
-Company fetcher registry.
-Add Playwright-based fetchers here as they are implemented.
+公司新聞爬蟲
 """
 
-FETCHERS = {}
+from .base import CompanyFetcher, CompanyDocument
+
+from .array import ArrayFetcher
+from .canadian_solar import CanadianSolarFetcher
+from .daqo import DaqoFetcher
+from .enphase import EnphaseFetcher
+from .first_solar import FirstSolarFetcher
+from .gcl import GclFetcher
+from .ja_solar import JaSolarFetcher
+from .jinko import JinkoFetcher
+from .longi import LongiFetcher
+from .maxeon import MaxeonFetcher
+from .meyer_burger import MeyerBurgerFetcher
+from .motech import MotechFetcher
+from .nextracker import NextrackerFetcher
+from .risen import RisenFetcher
+from .solaredge import SolaredgeFetcher
+from .sungrow import SungrowFetcher
+from .tongwei import TongweiFetcher
+from .trina import TrinaFetcher
+from .xinyi_solar import XinyiSolarFetcher
+from .yuan_jing import YuanJingFetcher
+
+FETCHERS = {
+    "array": ArrayFetcher,
+    "canadian_solar": CanadianSolarFetcher,
+    "daqo": DaqoFetcher,
+    "enphase": EnphaseFetcher,
+    "first_solar": FirstSolarFetcher,
+    "gcl": GclFetcher,
+    "ja_solar": JaSolarFetcher,
+    "jinko": JinkoFetcher,
+    "longi": LongiFetcher,
+    "maxeon": MaxeonFetcher,
+    "meyer_burger": MeyerBurgerFetcher,
+    "motech": MotechFetcher,
+    "nextracker": NextrackerFetcher,
+    "risen": RisenFetcher,
+    "solaredge": SolaredgeFetcher,
+    "sungrow": SungrowFetcher,
+    "tongwei": TongweiFetcher,
+    "trina": TrinaFetcher,
+    "xinyi_solar": XinyiSolarFetcher,
+    "yuan_jing": YuanJingFetcher,
+}
